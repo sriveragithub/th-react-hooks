e3217aa709bf83e5aae1b4a7c62ac28a3a3fb812
Skyler Rivera <skylerrivera17@yahoo.com>
2021-09-13 14:38:48 -0400

    demonstrated useEffect()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [score, setScore] = useState(0)
-  const [message, setMessage] = useState('Welcome!')
+  const [message, setMessage] = useState('Welcome')
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect called!');   
+    document.title = `${message}. Your score is ${score}`
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setData(data.message))
+      .catch(err => console.log('Oh noes!', err))
+  }, [message, score]);
 
   return (
     <div className="App">
@@ -16,6 +27,10 @@ function App() {
         <button onClick={() => setScore(prevScore => prevScore - 1)}>
           Decrease Score
         </button>
+        <button onClick={() => setScore(0)}>
+          Reset Score
+        </button>
+        <img src={data} alt="A random dog breed" />
       </header>
     </div>
   );
