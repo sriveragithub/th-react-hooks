af48a5d93959c706b44f495dbeb32522b6bb9f02
Skyler Rivera <skylerrivera17@yahoo.com>
2021-09-13 14:26:09 -0400

    demonstrating useState()

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 function App() {
   const [score, setScore] = useState(0)
+  const [message, setMessage] = useState('Welcome!')
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{ message }</h1>
         <h1>{ score }</h1>
-        <button onClick={() => setScore(score + 1)}></button>
+        <button onClick={() => setScore(prevScore => prevScore + 1)}>
+          Increase Score
+        </button>
+        <button onClick={() => setScore(prevScore => prevScore - 1)}>
+          Decrease Score
+        </button>
       </header>
     </div>
   );
